df5b037ff7cc86b8a11ee5dbe103051fe99239be
Okhrymenko <Ohrimenko15@gmail.com>
2025-08-04 10:38:47 +0300

    add apps user and borrowings to settings and to global urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
         "api/library/", include("library.urls", namespace="library")
     ),  # ✅ новий рядок
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowing")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
